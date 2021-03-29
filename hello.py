131078956b1380b1f0f0012de58d9049b67fcfbf
Voolodimer <vladimirbruho@gmail.com>
2021-03-29 20:49:54 +0300

    change in 'hello.py'

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 def wsgi_app(environ, start_response):
-    body = environ['QUERY_STRING'].split('&')
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = [bytes(i + '\n', ascii()) for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers)
     return body
 
 # 'sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8&client=ubuntu'
