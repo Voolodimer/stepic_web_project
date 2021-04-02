9d7dd102cd748e8e71933589fc5f30f40336639e
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-02 07:09:51 +0300

    change in urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
+    path('/login/', admin.site.urls),
+    path('/signup/', admin.site.urls),
+    path('/question/id/', admin.site.urls),
+    path('/ask/', admin.site.urls),
+    path('/popular/', admin.site.urls),
+    path('/new/', admin.site.urls),
 ]
