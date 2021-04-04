34452d30d4f3c6bf7df19b828c6044a9c3d970eb
Voolodimer <vladimirbruho@gmail.com>
2021-04-04 21:43:30 +0300

    update 'help.sh'

--- help.sh
+++ help.sh
@@ -1,15 +1,8 @@
-sudo apt purge python2.7-minimal
-sudo apt update
-sudo apt install python3.5
-sudo rm /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install gunicorn
-
 sudo apt-get update
-sudo apt-get install -y python3.5
-sudo apt-get install -y python3.5-dev
+sudo apt-get install -y python3.7
+sudo apt-get install -y python3.7-dev
 sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo ln -s /usr/bin/python3.7 /usr/bin/python3
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
