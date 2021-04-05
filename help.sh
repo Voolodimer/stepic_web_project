b7a77064af027871583af9cb15613fff7ffe66c8
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-05 10:04:12 +0300

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,10 +1,11 @@
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
 sudo apt-get install python3-pip python3.7-dev
 sudo apt-get install python3.7
 
-# sudo apt install software-properties-common
-# sudo add-apt-repository ppa:deadsnakes/ppa
+
 # sudo apt install python3.7
 #python3.7 --version
 #sudo apt-get update
