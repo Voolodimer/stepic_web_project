109980ec6a624f42dd08090e8569e66d9d511d5f
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-06 19:41:07 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from qa import views
 
 urlpatterns = [
-    path('admin/', ask.qa.views.test),
-    path('/', admin.site.urls),
-    path('/login/', admin.site.urls),
-    path('/signup/', admin.site.urls),
-    path(r'^question/\d+/$', admin.site.urls),
-    path('/ask/', admin.site.urls),
-    path('/popular/', admin.site.urls),
-    path('/new/', admin.site.urls),
+    path('admin/', views.test),
+    path('/', views.test),
+    path('/login/', views.test),
+    path('/signup/', views.test),
+    path(r'^question/\d+/$', views.test),
+    path('/ask/', views.test),
+    path('/popular/', views.test),
+    path('/new/', views.test),
 ]
