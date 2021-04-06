dcdb135ccbe4d2bfa88dd8c105f45f63abdab930
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-06 09:51:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 
-# sudo rm /etc/nginx/sites-enabled/default
-# sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 sudo gunicorn -c /etc/gunicorn.d/gunicorn-django.conf ask.wsgi:application
