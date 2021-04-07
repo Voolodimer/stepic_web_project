0c0244e6f6d7d093c39b9a75713ceccefa8f2335
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-07 09:26:49 +0300

    Update urls.py
    add "from django.conf.urls import url, include " in ask/ask/url.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import include
-import url
-import admin
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
