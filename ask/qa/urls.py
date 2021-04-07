b922dc225f7a91f1bb8d2ee66162adc75d480ef7
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-04-07 17:56:01 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,14 @@
-from django.contrib import admin
-from django.urls import path
-from qa import views
+# from django.contrib import admin
+# from django.urls import path
+# from qa import views
 
-urlpatterns = [
-    path('admin/', views.test),
-    path('/', views.test),
-    path('/login/', views.test),
-    path('/signup/', views.test),
-    path(r'^question/\d+/$', views.test),
-    path('/ask/', views.test),
-    path('/popular/', views.test),
-    path('/new/', views.test),
-]
+# urlpatterns = [
+#     path('admin/', views.test),
+#     path('/', views.test),
+#     path('/login/', views.test),
+#     path('/signup/', views.test),
+#     path(r'^question/\d+/$', views.test),
+#     path('/ask/', views.test),
+#     path('/popular/', views.test),
+#     path('/new/', views.test),
+# ]
