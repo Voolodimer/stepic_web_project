aa9c38eb854d0e8a272ec60c8f683b2e8204af1e
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-05-02 18:38:48 +0300

    Update help.sh

--- help.sh
+++ help.sh
@@ -19,14 +19,30 @@
 # sudo pip3 install --upgrade django==2.1
 # sudo pip3 install --upgrade gunicorn
 
-sudo /etc/init.d/mysql start
-sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt install python3.6
-sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.6 /usr/bin/python3
-curl "https://bootstrap.pypa.io/ez_setup.py" -o "ez_setup.py" && curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-sudo python3 ez_setup.py && sudo python3 get-pip.py
-sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
+# sudo /etc/init.d/mysql start
+# sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt install python3.6
+# sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+# curl "https://bootstrap.pypa.io/ez_setup.py" -o "ez_setup.py" && curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+# sudo python3 ez_setup.py && sudo python3 get-pip.py
+# sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
 
-sudo apt-get update
-sudo apt-get install python-pip python-dev mysql-server libmysqlclient-dev
-sudo apt-get install python3-dev
-sudo pip3 install django mysqlclient
+# sudo apt-get update
+# sudo apt-get install python-pip python-dev mysql-server libmysqlclient-dev
+# sudo apt-get install python3-dev
+# sudo pip3 install django mysqlclient
+
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
+sudo python3 -m pip install mysqlclient
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+python3 manage.py makemigrations qa
+python3 manage.py migrate
