5be1c10645a81d73235c3e900e4f5402128c499e
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-05-02 19:13:02 +0300

    Update help.sh

--- help.sh
+++ help.sh
@@ -20,11 +20,14 @@
 # sudo pip3 install --upgrade gunicorn
 
 
-# sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt install python3.6
-# sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.6 /usr/bin/python3
-# curl "https://bootstrap.pypa.io/ez_setup.py" -o "ez_setup.py" && curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-# sudo python3 ez_setup.py && sudo python3 get-pip.py
-# sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
+sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt install python3.6
+sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+curl "https://bootstrap.pypa.io/ez_setup.py" -o "ez_setup.py" && curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python3 ez_setup.py && sudo python3 get-pip.py
+sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
 
 # sudo apt-get update
 # sudo apt-get install python-pip python-dev mysql-server libmysqlclient-dev
@@ -48,11 +51,8 @@
 # sudo pip3 install --upgrade django==2.1
 # sudo pip3 install --upgrade gunicorn
 
-virtualenv -p python3 myvenv
-source myvenv/bin/activate
-pip install --upgrade pip
-pip install django
-pip install gunicorn
+# virtualenv -p python3 myvenv
+# source myvenv/bin/activate
 
 
 sudo python3 -m pip install mysqlclient
