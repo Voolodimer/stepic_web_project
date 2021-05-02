48d15f9114190cf6012741c82870580aceb9c635
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-05-02 22:41:23 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,36 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
+
 class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, related_name="question_author", on_delete=models.CASCADE, default=1)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
+   title = models.CharField(max_length=255)
+   text = models.TextField()
+   added_at = models.DateTimeField(auto_now=True)
+   rating = models.IntegerField(default=0)
+   author = models.ForeignKey(User, related_name='question_author')
+   likes = models.ManyToManyField(User, related_name='question_likes')
 
 
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-
-class QuestionManager(models.Manager):
-    def new(self):
-        """
-        метод возвращающий последние добавленные вопросы
-        """
-        return self.order_by('-added_at')
-
-
-    def popular(self):
-        """
-        метод возвращающий вопросы отсортированные по рейтингу
-        """
-        return self.order_by('-rating')
-
-# class User(django.contrib.auth.models.User):
-#     pass
-
-# Create your models here.
+   text = models.TextField()
+   added_at = models.DateTimeField(auto_now=True)
+   question = models.ForeignKey(Question)
+   author = models.ForeignKey(User, related_name='answer_author')
