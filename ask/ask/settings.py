25f11058fe25c8947355a82eadb4de8c55e4ef27
Voolodimer <39840620+Voolodimer@users.noreply.github.com>
2021-05-03 15:43:57 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'stepic_web',
+        'NAME': 'stepic_web',
     }
 }
 # 'NAME': BASE_DIR / 'db.sqlite3',
